2b2f10aeeb070954ac3d77a651426e8a035f575b
jesua <jesua@JESUA>
2025-01-22 18:57:05 -0600

    Agregar archivos de proyecto.

---
+++ Clases-Q12025/Ave.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_Q12025
+{
+    public class Ave
+    {
+
+        public string? Nombre { get; set; }
+        public string? Especie { get; set; }
+        public string? Color {  get; set; }
+        public double Tamanio { get; set; }
+        public string? Habitat { get; set; }
+
+        //Metodo
+        public void Imprimir()
+        {
+            Console.WriteLine($"Nombre: {Nombre}");  //Interpolación $"Dato : {Variable}"
+            Console.WriteLine($"Especie : {Especie}");
+            Console.WriteLine($"Color : {Color}");
+            Console.WriteLine($"Tamaño : {Tamanio}");
+            Console.WriteLine($"Habitat : {Habitat}");
+            Console.WriteLine("");
+            
+        }
+
+
+    }
+}
---
+++ Clases-Q12025/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Clases_Q12025;
+
+Console.WriteLine("Clases --Aves--");
+
+
+Ave ave = new Ave();
+
+
+ave.Nombre = "Loro";
+ave.Especie = "Amazona aestiva";
+ave.Color = "Verde";
+ave.Tamanio = 30.5;
+ave.Habitat = "Bosques Tropicales";
+
+
+ave.Imprimir();
